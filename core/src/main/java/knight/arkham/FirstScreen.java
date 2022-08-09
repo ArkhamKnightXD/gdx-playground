80335a5ec5d633a9db25c8a8db9d6870641119bf
kevin <karvin_jimenez@hotmail.com>
2022-08-08 22:48:11 -0400

    extra changes

--- core/src/main/java/knight/arkham/FirstScreen.java
+++ core/src/main/java/knight/arkham/FirstScreen.java
@@ -4,7 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 //import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FirstScreen extends ScreenAdapter {
@@ -24,6 +28,11 @@ public class FirstScreen extends ScreenAdapter {
 	private int textureHeight;
 
 	private final int playerSpeed;
+	
+	private final Sound clickSound;
+
+	private final Array<Rectangle> randomRectangles;
+	
 //	private final OrthographicCamera camera;
 
 
@@ -39,6 +48,9 @@ public class FirstScreen extends ScreenAdapter {
 
 		screenClickCounter = 0;
 		playerSpeed = 250;
+		clickSound = Gdx.audio.newSound(Gdx.files.internal("fx/drop.wav"));
+
+		randomRectangles = new Array<>();
 	}
 
 	@Override
@@ -64,25 +76,66 @@ public class FirstScreen extends ScreenAdapter {
 		game.font.draw(game.batch, "Screen Height: " + game.getScreenHeight(), 275, 180);
 		game.font.draw(game.batch, "Screen Width: " + game.getScreenWidth(), 275, 150);
 
+		for (Rectangle rectangle: randomRectangles) {
+
+			game.batch.draw(initialTexture, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+		}
+
 
 		game.batch.end();
 
-		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
-			Gdx.app.exit();
+		if (Gdx.input.justTouched()){
 
-		if (Gdx.input.justTouched())
+			clickSound.play(0.1f);
+
 			screenClickCounter++;
+		}
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.R))
+			generateRandomRectangles();
+
 
 		if (Gdx.input.isTouched()){
 
+//			Las posiciones del mouse empiezan en la esquina superior izquierda, al contrario, de cuando dibujo con el
+//			batch que su punto de origen es la esquina inferior izquierda
 			mousePositionX = Gdx.input.getX();
 			mousePositionY = Gdx.input.getY();
 
+//			positionX = mousePositionX;
+//			positionY = mousePositionY;
+
 			textureWidth++;
 			textureHeight++;
+
 		}
 
 		playerMovement(delta);
+
+		closeGame();
+
+	}
+
+
+	private void generateRandomRectangles(){
+
+		Rectangle rectangle = new Rectangle();
+
+		rectangle.width = 16;
+		rectangle.height = 16;
+		rectangle.x = MathUtils.random(0, 640-16);
+		rectangle.y = MathUtils.random(0, 480-16);
+
+		randomRectangles.add(rectangle);
+	}
+
+	private void closeGame() {
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)){
+
+			Gdx.app.exit();
+			dispose();
+		}
 	}
 
 	private void playerMovement(float deltaTime) {
@@ -131,5 +184,7 @@ public class FirstScreen extends ScreenAdapter {
 	@Override
 	public void dispose() {
 
+		initialTexture.dispose();
+		clickSound.dispose();
 	}
 }
