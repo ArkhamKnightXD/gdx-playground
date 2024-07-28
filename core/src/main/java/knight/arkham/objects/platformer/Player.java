adc735f19a38b7e39753ea4222bbe01cf7070dc6
knight <karvin_jimenez@hotmail.com>
2024-07-28 04:50:57 -0400

    extra changes.

--- core/src/main/java/knight/arkham/objects/platformer/Player.java
+++ core/src/main/java/knight/arkham/objects/platformer/Player.java
@@ -4,29 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class Player extends GameObject {
 
-    public boolean isPlayerGrounded;
+    public boolean isPlayerGrounded = false;
+    private float impulse = 20000;
+    private float gravity = 0;
+    private float gravityIncrement = -400;
+//    private final Vector2 velocity = new Vector2(0,0);
 
     public Player(Rectangle bounds) {
         super(bounds, new Texture("images/initial.png"), 350);
-
-        isPlayerGrounded = false;
     }
 
-    public void update(float delta) {
+    public void update(float deltaTime) {
 
-        if (!isPlayerGrounded)
-            bounds.y -= speed * delta;
+        if (!isPlayerGrounded || Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            bounds.y += gravity * deltaTime;
+            gravity += gravityIncrement * deltaTime;
+        }
 
         if (Gdx.input.isKeyPressed(Input.Keys.D))
-            bounds.x += speed * delta;
+            bounds.x += speed * deltaTime;
 
         else if (Gdx.input.isKeyPressed(Input.Keys.A))
-            bounds.x -= speed * delta;
+            bounds.x -= speed * deltaTime;
 
         if (isPlayerGrounded && Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
-            bounds.y += 4000 * delta;
+            gravity = impulse * deltaTime;
     }
 }
